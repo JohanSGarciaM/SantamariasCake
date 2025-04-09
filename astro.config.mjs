575952a5994ce8b00a8b9dc44e42882a49ed3819
unknown <johangarciats2015@gmail.com>
2025-04-08 23:22:33 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://johansgarciam.github.io",
+  base: "santamariascake",
 });
